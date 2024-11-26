337aa319c3334eb3985218f39d8c55c433b41a46
Reza Abbaspourshirazifard <shirazi.reza.sh@gmail.com>
2024-11-25 21:59:11 -0800

    somestyling

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import CitySearch from './CitySearch'
+import CitySearch from './CitySearch';
 
 export default function MainContent({ user, isOpen }) {
-
     const navigate = useNavigate();
 
     const handleCitySelect = (city) => {
@@ -16,14 +15,14 @@ export default function MainContent({ user, isOpen }) {
         navigate(`/report/generate/${encodedLocation}`, {
             state: { location: city }
         });
-    }
+    };
 
     return (
-        <div className="max-w-5xl mx-auto px-4 mt-32">
-            <div className="max-w-5xl w-full px-4">
+        <div className="w-full max-w-5xl mx-auto px-0 sm:px-4 mt-32">
+            <div className="w-full">
                 <div className="content-center space-y-8 justify-center items-center">
                     <div className="start-report text-center flex flex-col">
-                        <div className="flex-1 flex flex-col justify-center mb-12"> {/* Reduced bottom margin */}
+                        <div className="flex-1 flex flex-col justify-center mb-12">
                             <h1 className="start-report-first-label">
                                 Generate a neighbourhood report
                             </h1>
@@ -31,12 +30,12 @@ export default function MainContent({ user, isOpen }) {
                                 Get instant market insights to share with your clients.
                             </p>
                         </div>
-                        <div className="transform -translate-y-4"> {/* Moved up with negative translate */}
+                        <div className="transform -translate-y-4 px-4 sm:px-0">
                             <CitySearch onSelect={handleCitySelect} onClicked={handleClick} />
                         </div>
                     </div>
 
-                    <div className="mt-16"> {/* Adjusted margin for reports section */}
+                    <div className="mt-16 px-4 sm:px-0">
                         <h2 className="text-xl font-semibold text-blue-900 mb-4">My reports</h2>
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                             No Reports were found!
